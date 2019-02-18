b7b6c55aec10b82cbf4c1fb5865c2cc28b9e55ee
Kubing Lin <edkt973@gmail.com>
2019-02-18 16:42:03 +0800

    refactoring UOW

--- Repository Pattern.Repository/UnitOfWork/IUnitOfWork.cs
+++ Repository Pattern.Repository/UnitOfWork/IUnitOfWork.cs
@@ -4,12 +4,8 @@ namespace Repository_Pattern.Repository
 {
     public interface IUnitOfWork : IDisposable
     {
-        ICourseRepository Course { get; }
-        IAuthorRepository Author { get; }
-        IBookRepository Book { get; }
-
         int Commit();
 
-        void RejectChanges();
+        void RollBack();
     }
 }
--- Repository Pattern.Repository/UnitOfWork/UnitOfWork.cs
+++ Repository Pattern.Repository/UnitOfWork/UnitOfWork.cs
@@ -1,33 +1,26 @@
-﻿using System;
+﻿using Repository_Pattern.Entity;
+using System;
 using System.Data.Entity;
 using System.Linq;
 
 namespace Repository_Pattern.Repository
 {
-    public class UnitOfWork<TDbContext> : IUnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
         private DbContext _context { get; }
         private bool _disposed;
 
-        public UnitOfWork(
-            IDbContextFactory contextFactory)
+        public UnitOfWork(DbContext context)
         {
-            _context = contextFactory.GetDbContext<TDbContext>();
-            Course = new CourseRepository(_context);
-            Author = new AuthorRepository(_context);
-            Book = new BookRepository(_context);
+            _context = context;
         }
-
-        public ICourseRepository Course { get; private set; }
-        public IAuthorRepository Author { get; private set; }
-        public IBookRepository Book { get; private set; }
 
         public int Commit()
         {
             return _context.SaveChanges();
         }
 
-        public void RejectChanges()
+        public void RollBack()
         {
             foreach (var entry in _context.ChangeTracker.Entries()
               .Where(e => e.State != EntityState.Unchanged))
