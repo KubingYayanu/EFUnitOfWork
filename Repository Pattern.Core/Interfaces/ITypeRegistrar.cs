014e17c2466af233843981f4b4f894f672a47699
Kubing Lin <edkt973@gmail.com>
2019-02-19 13:18:15 +0800

    add Core Project

---
+++ Repository Pattern.Core/Interfaces/ITypeRegistrar.cs
@@ -0,0 +1,11 @@
+﻿using Autofac;
+
+namespace Repository_Pattern.Core
+{
+    public interface ITypeRegistrar
+    {
+        int Order { get; }
+
+        void RegisterTypes(ContainerBuilder builder);
+    }
+}
